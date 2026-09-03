41a3a989c17ef917344b8a87412b84a3f650a6c2
rizz <matveenko.semen09@gmail.com>
2026-09-03 16:21:15 +0700

    start project

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Start project!",
         modifier = modifier
     )
 }
